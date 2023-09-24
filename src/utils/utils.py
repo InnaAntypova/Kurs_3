d205209ed6082efd4271626008ac73032e70456f
Inna Antypova <nightallatiel@gmail.com>
2023-09-24 16:35:21 +0300

    add some defs to utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,6 @@
 import json
+from operator import itemgetter
+import datetime
 
 
 def load_operations_json(file_path):
@@ -16,5 +18,26 @@ def load_operations_json(file_path):
             else:
                 list_operations.append(f)
         return list_operations
+
+
+def sorted_operations(lst_operations):
+    """
+    Функция сортирует операции по ключам
+    :param lst_operations:
+    :return: отсортированный список
+    """
+    sort_date = sorted(lst_operations, key=itemgetter('date'), reverse=True)
+    sort_operations = sorted(sort_date, key=itemgetter('state'), reverse=True)
+    return sort_operations
+
+
+def get_date(i):
+    """
+    Функция возвращает дату в нужном шаблоне
+    """
+    date_time_str = i['date']
+    date_time_obj = datetime.datetime.strptime(date_time_str, "%Y-%m-%dT%H:%M:%S.%f")
+    date = date_time_obj.date().strftime('%d.%m.%Y')
+    return date
 
 
