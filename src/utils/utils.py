84e9a654eef7df5e93daa0768bfdfd94135407f7
Inna Antypova <nightallatiel@gmail.com>
2023-09-24 16:36:24 +0300

    add defs to utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -41,3 +41,41 @@ def get_date(i):
     return date
 
 
+def hide_number_from(card_number_from):
+    """
+    Функция кодирует звездочкой номер карты Отправителя
+    """
+    hide_number_from_ = card_number_from[0:4] + ' ' + card_number_from[4:6] + '*' * len(card_number_from[6:-4]) + \
+                        card_number_from[-4:]
+    return hide_number_from_
+
+
+def hide_number_to(card_number_to):
+    """
+    Функция кодирует звездочкой номер карты Получателя
+    """
+    hide_number_to_ = '*' * len(card_number_to[-6:-4]) + card_number_to[-4:]
+    return hide_number_to_
+
+
+def get_feedback(i, date):
+    """
+    Функция генерирует сообщение с информацией об операциях
+    """
+    print(f"{date} {i['description']}")
+    if 'from' not in i:  # если нет ключа 'from'
+        card = i['to'].split()
+        card_number_to = card[-1]
+        number_to = hide_number_to(card_number_to)
+        print(f" > {' '.join(card[:-1])} {number_to}")
+    else:
+        card = i['to'].split()
+        card_number_to = card[-1]
+        number_to = hide_number_to(card_number_to)
+        card_ = i['from'].split()
+        card_number_from = card_[-1]
+        number_from = hide_number_from(card_number_from)
+        print(f"{' '.join(card_[:-1])} {number_from} > {' '.join(card[:-1])} {number_to}")
+
+    print(f"{i['operationAmount']['amount']} {i['operationAmount']['currency']['name']}")
+    print(" ")
